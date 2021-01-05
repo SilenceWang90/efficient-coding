1df2dec3a725d53c159034d7b23b3739664fc93c
wangpeng116 <wangpeng116@longfor.com>
2021-01-05 14:28:45 +0800

    初始化项目

---
+++ src/main/java/com/wp/EfficientCodingApplication.java
@@ -0,0 +1,17 @@
+package com.wp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Classname EfficientCoding
+ * @Description 启动类
+ * @Date 2021/1/5 13:47
+ * @Created by wangpeng116
+ */
+@SpringBootApplication
+public class EfficientCodingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EfficientCodingApplication.class, args);
+    }
+}
