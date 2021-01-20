21a08ff9bd72c0d6cadf144f80696034197e22c5
wangpeng116 <wangpeng116@longfor.com>
2021-01-20 15:41:00 +0800

    Set工具处理

---
+++ src/main/java/com/wp/guava/SetsTest.java
@@ -0,0 +1,43 @@
+package com.wp.guava;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.Sets;
+
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @Classname SetsTest
+ * @Description Set工具类
+ * @Date 2021/1/20 15:18
+ * @Created by wangpeng116
+ */
+public class SetsTest {
+    private static final Set<Integer> set1 = Sets.newHashSet(1, 2, 3);
+    private static final Set<Integer> set2 = Sets.newHashSet(4, 5, 6);
+
+    public static void main(String[] args) throws JsonProcessingException {
+        // 并集
+        Set<Integer> setTest1 = Sets.union(set1, set2);
+        System.out.println(setTest1);
+        // 交接
+        Set<Integer> setTest2 = Sets.intersection(set1, set2);
+        System.out.println(setTest2);
+        // 差集(参数1有而参数2没有)
+        Set<Integer> setTest3 = Sets.difference(set1, set2);
+        Set<Integer> setTest4 = Sets.difference(set2, set1);
+        System.out.println(setTest3);
+        System.out.println(setTest4);
+        // 相对差集(A,B非交集部分的元素，即属于A但不属于B以及属于B但不属于A的元素)
+        Set<Integer> setTest5 = Sets.symmetricDifference(set1, set2);
+        System.out.println(setTest5);
+        // 拆解成各种组合的子集
+        ObjectMapper objectMapper = new ObjectMapper();
+        Set<Set<Integer>> setTest6 = Sets.powerSet(set1);
+        System.out.println(objectMapper.writeValueAsString(setTest6));
+        // 笛卡尔积
+        Set<List<Integer>> set7 = Sets.cartesianProduct(set1,set2);
+        System.out.println(set7);
+    }
+}
