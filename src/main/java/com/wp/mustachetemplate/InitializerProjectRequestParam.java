94ae969c97a4f448a142ed2b0961947db0330113
wangpeng <wangpeng0717@sina.cn>
2023-10-18 17:36:03 +0800

    Mustache模板引擎

--- src/main/java/com/wp/mustachetemplate/InitializerProjectRequestParam.java
+++ src/main/java/com/wp/mustachetemplate/InitializerProjectRequestParam.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -17,12 +18,15 @@ import java.util.Map;
 @NoArgsConstructor
 @Accessors(chain = true)
 public class InitializerProjectRequestParam {
-    // 组件类型
+    // 1、组件类型
     private String componentType;
-    // 模板名称
+    // 2、模板名称
     private String mustacheTemplateName;
-    // 参数信息
+    // 3、参数信息
+    // 3.1、普通类型参数变量
     private Map<String, Object> params;
-    // 文件输出地址
+    // 3.2、集合类型参数变量：目前仅pom渲染时会用到此参数
+    private List<Map<String, Object>> collectionParams;
+    // 4、文件输出地址
     private String outputUri;
 }
