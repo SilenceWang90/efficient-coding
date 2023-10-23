f790352b841f0248557916b217f7264345f2c5dc
wangpeng <wangpeng0717@sina.cn>
2023-10-23 17:31:51 +0800

    Mustache模板引擎

--- src/main/java/com/wp/mustachetemplate/InitializerProjectRequestParam.java
+++ src/main/java/com/wp/mustachetemplate/InitializerProjectRequestParam.java
@@ -23,6 +23,8 @@ public class InitializerProjectRequestParam {
     private String mustacheTemplateName;
     // 3、参数信息
     private Map<String, Object> params;
-    // 4、文件输出地址
-    private String outputUri;
+    // 4、文件所在目录
+    private String directoryPath;
+    // 5、文件绝对路径
+    private String filePath;
 }
