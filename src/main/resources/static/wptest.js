bd8e9a3f660157e0a210bd5dc65086c057103868
wangpeng <wangpeng0717@sina.cn>
2024-06-25 13:22:41 +0800

    国际化处理

---
+++ src/main/resources/static/wptest.js
@@ -0,0 +1,13 @@
+/** 请求头放入lang参数 **/
+var testi18n = function () {
+    $.ajax({
+        url: "/test-i18n/getSpecificI18nInfo?specific-keys=greet",
+        type: "GET",
+        headers: {
+            "lang": "en"
+        },
+        success: function (response) {
+            alert("返回的信息为：" + response.greet);
+        }
+    })
+}();
