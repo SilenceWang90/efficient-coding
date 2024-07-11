be0fc1d1541716bbb7a7cb9f8a3abc5777ad77e9
wangpeng <wangpeng0717@sina.cn>
2024-07-11 10:52:29 +0800

    GitTest第一次提交

---
+++ src/main/java/com/wp/dto/GitTest.java
@@ -0,0 +1,12 @@
+package com.wp.dto;
+
+/**
+ * @author wangpeng
+ * @description GitTest
+ * @date 2024/7/11 09:57
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("第一次提交");
+    }
+}
