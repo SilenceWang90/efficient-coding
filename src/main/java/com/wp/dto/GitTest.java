d835feca8232e302d2691e03bdc6a1b660669e20
wangpeng <wangpeng0717@sina.cn>
2024-07-13 09:04:28 +0800

    GitTest：第二十八次提交

--- src/main/java/com/wp/dto/GitTest.java
+++ src/main/java/com/wp/dto/GitTest.java
@@ -33,5 +33,6 @@ public class GitTest {
         System.out.println("第二十五次提交");
         System.out.println("第二十六次提交");
         System.out.println("第二十七次提交");
+        System.out.println("第二十八次提交");
     }
 }
