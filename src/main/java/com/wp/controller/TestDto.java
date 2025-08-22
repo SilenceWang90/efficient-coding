5cd6ce57328e7433e7d35aa86d8b9b0c73fa3186
wangpeng <wangpeng0717@sina.cn>
2025-08-22 17:35:19 +0800

    sse发送对象数据自动序列化

--- src/main/java/com/wp/controller/TestDto.java
+++ src/main/java/com/wp/controller/TestDto.java
@@ -1,6 +1,8 @@
 package com.wp.controller;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.experimental.Accessors;
 
 /**
  * @author wangpeng
@@ -8,6 +10,8 @@ import lombok.Data;
  * @date 2025/8/18 08:38
  **/
 @Data
+@Accessors(chain = true)
+@AllArgsConstructor
 public class TestDto {
     private String name;
     private int age;
