87a41e14715cd41a80751dca7dbe47e495c06121
wangpeng <wangpeng0717@sina.cn>
2026-01-31 14:23:46 +0800

    创建、验证 验证码方法

--- src/main/java/com/wp/controller/CaptchaController.java
+++ src/main/java/com/wp/controller/CaptchaController.java
@@ -4,9 +4,7 @@ import com.wp.util.CaptchaUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.io.IOException;
@@ -55,6 +53,7 @@ public class CaptchaController {
             String captchaId = UUID.randomUUID().toString();
             // 分布式集群环境中要将验证码的信息存储在redis中，便于后续的验证
             stringRedisTemplate.opsForValue().set(captchaId, String.valueOf(mathExpression.getResult()), Duration.ofSeconds(20));
+            /** 3、封装图片信息返回值 **/
             captchaResponse.setCaptchaId(captchaId);
             captchaResponse.setImageData(imageBase64);
             return captchaResponse;
@@ -63,20 +62,34 @@ public class CaptchaController {
         }
     }
 
-//    /**
-//     * 验证答案
-//     * @param answer 用户输入的答案
-//     * @return 验证结果
-//     */
-//    @PostMapping("/validate")
-//    @ResponseBody
-//    public ResponseEntity<String> validateCaptcha(@RequestParam String answer) {
-//        System.out.println("收到验证请求，答案: " + answer);
-//        boolean isValid = captchaService.validateCaptcha(answer);
-//        if (isValid) {
-//            return ResponseEntity.ok("验证成功");
-//        } else {
-//            return ResponseEntity.badRequest().body("验证失败");
-//        }
-//    }
+    /**
+     * 验证答案
+     *
+     * @param captchaId  验证码图片id
+     * @param userAnswer 用户输入的答案
+     * @return 验证结果
+     */
+    @PostMapping("/validateCaptcha")
+    @ResponseBody
+    public Boolean validateCaptcha(@RequestParam("captchaId") String captchaId, @RequestParam("answer") String userAnswer) {
+        try {
+            /** 1、获取验证码正确答案 **/
+            int correctAnswer = Integer.parseInt(stringRedisTemplate.opsForValue().get("captchaId"));
+            int answer = Integer.parseInt(userAnswer);
+            System.out.println("验证码ID: " + captchaId);
+            System.out.println("用户输入答案: " + answer);
+            System.out.println("正确答案: " + correctAnswer);
+            /** 2、验证答案是否正确 **/
+            if (answer == correctAnswer) {
+                System.out.println("验证成功");
+                return true;
+            } else {
+                System.out.println("验证失败");
+                return false;
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("数字格式错误: " + userAnswer);
+            return false;
+        }
+    }
 }
